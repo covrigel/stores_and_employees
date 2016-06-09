5ff133fe7ba98b04cba903b3f27aa56ad974e53a
covrigel <alexandra.nemes1986@gmail.com>
2016-06-09 01:33:32 +0000

    exercises 1-7(without b., c., d., from 7)

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create!(name: "Surrey", annual_revenue: 224_000, mens_apparel: false, womens_apparel: true)
+Store.create!(name: "Whistler", annual_revenue: 1_900_000, mens_apparel: true, womens_apparel: false)
+Store.create!(name: "Yaletown", annual_revenue: 430_000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "Men's stores: #{store.name} makes an annual revenue of #{store.annual_revenue}" 
+end
+
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue <= ?", 1_000_000)
+@womens_stores.each do |store|
+  puts "Women's stores: #{store.name}'s annual revenue is #{store.annual_revenue}"
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts @total_revenue
+
+@average_revenue = Store.average(:annual_revenue)
+puts @average_revenue
+
+@very_profitable = Store.where("annual_revenue >= ?", 1_000_000).count
+puts @very_profitable
